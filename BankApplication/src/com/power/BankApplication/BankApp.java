97e27ce1ce2d71f1616a4ca90881d0ca9a119ee9
Gauri Ragade <gauriragade98@gmail.com>
2023-01-30 19:05:31 +0530

    code cleanup

--- BankApplication/src/com/power/BankApplication/BankApp.java
+++ BankApplication/src/com/power/BankApplication/BankApp.java
@@ -5,17 +5,19 @@ import java.util.Scanner;
 
 public class BankApp {
 	public static void main(String args[]) {
+		
 		BankApp bankapp1 = new BankApp();
 
 		ArrayList<Customer> customers = new ArrayList<Customer>();
 		int choice = 0;
+		
 		while (choice != 9) {
 			System.out.print("--------------WELCOME-CITYBANK-----------------\n");
 
 			System.out.println("1. Customers ");
 			System.out.println("9. Exit");
-
 			System.out.print("Enter your choice: ");
+			
 			Scanner sc = new Scanner(System.in);
 			int choiceNew = sc.nextInt();
 
@@ -31,21 +33,24 @@ public class BankApp {
 
 				switch (choice1) {
 				case 1:
-
 					bankapp1.addCustomer(customers);
 					break;
+					
 				case 3:
 					bankapp1.showCustomerDetail(customers);
 					break;
 
 				default:
 					break;
+					
 				}
 
 				break;
+				
 			case 9:
 				choice = 9;
 				break;
+				
 			default:
 				System.out.println("Give right choice");
 				break;
@@ -54,8 +59,10 @@ public class BankApp {
 	}
 
 	private void addCustomer(ArrayList<Customer> customers) {
+		
 		System.out.println("\tFill-Up Cutomer information:-");
 		Customer customer1 = new Customer();
+		
 		System.out.print("\tEnter customer id: ");
 		Scanner sc = new Scanner(System.in);
 		int id = sc.nextInt();
@@ -81,19 +88,16 @@ public class BankApp {
 	}
 
 	private void showCustomerDetail(ArrayList<Customer> coustomers) {
+		
 		System.out.println("\t---------------------Customer Detailes: -----------------------");
 		System.out.println("\tId" + "\t" + "Name" + "\t" + "Address" + "\t" + "Phone number");
-		for (Customer customer : coustomers) {
-			System.out.println("\t"+customer.getCutomer_id() + "\t" + customer.getCustomer_name() + "\t"
-					+ customer.getCustomer_address() + "\t" + customer.getPhone_no() + "\t");
-			// System.out.println("Name: " + customer.customer_name);
-			// System.out.println("Address: "+customer.customer_address);
-			// System.out.println("phone Number: " + customer.phone_no);
 
-			// customer1.getCutomer_id();
-//		customer1.getCustomer_name();
-//		customer1.getCustomer_address();
+		for (Customer customer : coustomers) {
+			System.out.println("\t" + customer.getCutomer_id() + "\t" + customer.getCustomer_name() + "\t"
+					+ customer.getCustomer_address() + "\t" + customer.getPhone_no() + "\t");
+
 		}
+		
 		System.out.println("\t---------------------------------------------------------------");
 	}
 }
